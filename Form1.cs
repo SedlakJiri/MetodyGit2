9cac4608260c400a1111d7f363695e9888e48db3
SedlakJiri <Sedlakj.02@spst.eu>
2021-03-22 09:27:50 +0100

    Přidejte soubory projektu.

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetodyGit2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DateTime datum = DateTime.Now;
+            DateTime Za100Dni = datum.AddDays(100);
+            MessageBox.Show("Dnesni datum: " + datum);
+            MessageBox.Show("Datum za 100 dni: " + Za100Dni);
+        }
+    }
+}
